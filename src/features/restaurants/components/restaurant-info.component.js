0e4d03c83da1c6d693a8d38f78497a57b9343a05
Nicolas Corbalan <outreef@hotmail.com>
2021-04-04 15:19:21 +0200

    restaurant card list

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const RestaurantInfo = () => {
+  return (
+    <View>
+      <Text>Restaurant Info</Text>
+    </View>
+  );
+};
