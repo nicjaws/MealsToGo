92119ddf24b45cc0660384c3948c897a18a7ab57
Nicolas Corbalan <outreef@hotmail.com>
2021-05-18 14:22:19 +0200

    spacerView

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { styled } from "styled-components/native";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariant = {
   small: 1,
   medium: 2,
   large: 3,
-}
+};
 
 const positionVariant = {
-  top: 'marginTop',
-  left: 'marginLeft',
-  right: 'marginRight',
-  bottom: 'marginBottom',
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
 };
 
 const getVariant = (position, size, theme) => {
@@ -19,13 +19,20 @@ const getVariant = (position, size, theme) => {
   const property = positionVariant[position];
   const value = theme.space[sizeIndex];
   console.log(value);
-  return `${property} : ${value}`
-}
-export const Spacer = styled.View`
-  ${({position, size, theme}) => getVariant(position, size, theme)}
+  return `${property} : ${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
 `;
 
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
 Spacer.defaultProps = {
-  position = 'top',
-  size = 'small'
-}
+  position: "top",
+  size: "small",
+};
