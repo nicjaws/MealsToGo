45d9c187a246617056081f7719541c4b35fd23e3
Nicolas Corbalan <outreef@hotmail.com>
2021-06-23 20:04:55 +0200

    flatList

--- src/features/restaurants/components/restaurant-info-card.styles.js
+++ src/features/restaurants/components/restaurant-info-card.styles.js
@@ -4,6 +4,7 @@ import { Text } from "../../../typography/text.component";
 
 export const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantCardCover = styled(Card.Cover)`
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -4,7 +4,6 @@ import { StatusBar, FlatList, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { Item } from "react-native-paper/lib/typescript/components/List/List";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -15,16 +14,29 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <FlatList
-      data={[{ name: 1 }, { name: 2 }]}
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+        { name: 4 },
+        { name: 5 },
+        { name: 6 },
+      ]}
       renderItem={() => <RestaurantInfoCard />}
       keyExtractor={(item) => item.name}
-      contentContainerStyle={{padding: 16}}
+
     />
   </SafeArea>
 );
